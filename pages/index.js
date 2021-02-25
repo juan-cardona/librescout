db58a50483482631188edcc67e3a16bf70de4cf6
Juan Jose Cardona <joscardona@icloud.com>
2021-02-25 16:29:46 +0000

    adds tailwind config

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <h1 className="text-5xl text-yellow-300 text-center font-bold">
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
