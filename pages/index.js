b6871e85561b276811bd7c4988b79e7ae8b37478
Juan Jose Cardona <joscardona@icloud.com>
2021-02-26 01:46:34 +0000

    makes posisble to fetch data from ML API

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+import fetch from 'node-fetch'
+import {RestClientApi} from 'mercadolibre-nodejs-sdk'
+
+function Home(posts) {
   return (
     <div className={styles.container}>
       <Head>
@@ -10,44 +13,11 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+
         <h1 className="text-5xl text-yellow-300 text-center font-bold">
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
       <footer className={styles.footer}>
@@ -59,7 +29,28 @@ export default function Home() {
           Powered by{' '}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
+        <ul>
+      {posts.map((post) => (
+        <li>{post.text}</li>
+      ))}
+    </ul>
       </footer>
     </div>
   )
+      }
+
+
+
+const api = "https://api.mercadolibre.com/users/me?access_token=APP_USR-3249860603230326-022523-f6159a4e99f13474edaed0ad62f07e2b-531228956"
+
+export async function getStaticProps() {
+  const res = await fetch(api)
+  const data = await res.json()
+  console.log(data)
+  return {
+    props: { data },
+  }
 }
+
+
+export default Home
